6912d840be5c11102c40e117fee5988829f57c30
sararob <sararob@brandeis.edu>
2013-06-12 13:47:59 -0700

    Initial commit

---
+++ app/controllers/bdays_controller.rb
@@ -0,0 +1,5 @@
+class BdaysController < ApplicationController
+  def home
+
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bday::Application.config.secret_token = '297d3c7c9ac69ef23d0a143823050ba507a41cee52c650788af932e257176a8ada90e0d4abfa5cbba5268e04d74e3d673501951a6bf653242f5b47e00663be0b'
